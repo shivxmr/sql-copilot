ada218ec8a4ed249b7101de685047da14d8ee96c
Shivam Raj <shivamraj.me21@rvce.edu.in>
2024-01-15 03:11:49 +0530

    everything working fine

---
+++ src/components/loader/loader.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import './Loader.css';
+
+const Loader = () => (
+    <div className="boxes mt-10">
+        <div className="box">
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
+        <div className="box">
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
+        <div className="box">
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
+        <div className="box">
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
+    </div>
+);
+
+export default Loader;
